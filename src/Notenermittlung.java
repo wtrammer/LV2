a616c5a6dedbf9a072e3260cef505b2fc0fe6eca
wtrammer <wolfgang.trammer@nts.eu>
2015-11-03 18:53:57 +0100

    1

---
+++ src/Notenermittlung.java
@@ -0,0 +1,22 @@
+
+public class Notenermittlung {
+
+	
+	
+	public static void main(String[] args) {
+
+	int noten = 91;
+	
+		if (noten >= 90)
+		{
+			
+			System.out.println("Sehr Gut");
+			
+			
+		}
+		
+		
+		
+	}
+
+}
