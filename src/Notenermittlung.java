3e73e40613224da60b207d464b7f43c0d0a3c708
wtrammer <wolfgang.trammer@nts.eu>
2015-11-09 16:46:16 +0100

    a

--- src/Notenermittlung.java
+++ src/Notenermittlung.java
@@ -2,18 +2,41 @@
 public class Notenermittlung {
 
 	
-	
+	public static String note(int noten){
+		
+	} 
 	public static void main(String[] args) {
 
-	int noten = 91;
+	//int noten = 4;
+	//String note = "nix";
 	
 		if (noten >= 90)
 		{
-			
-			System.out.println("Sehr Gut");
-			
-			
+			note = "Sehr Gut";		
 		}
+		
+		else if (noten >=78)
+		{
+			note= "Gut";
+		}
+		
+		else if (noten >=65)
+		{
+			note = "Befriedigend";
+		}
+		
+		else if (noten >= 51)
+		{
+			note = "Genügend";
+		}
+		else 
+		{
+			note = "Nicht Genügend";
+		}
+		
+		
+		
+		System.out.println("Note = " + note);
 		
 		
 		
